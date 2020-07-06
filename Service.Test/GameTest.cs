abfbf9b62134840a9af9004f3694e779b708604a
ZhangNing <zhangning885@pingan.com.cn>
2020-07-06 09:48:12 +0800

    add multi combine test case

--- Service.Test/GameTest.cs
+++ Service.Test/GameTest.cs
@@ -78,7 +78,7 @@ namespace Service.Test
             const string expectCheckerboard = "[  ] [  ] [  ] [  ]\r\n[  ] [ 2] [  ] [  ]\r\n[  ] [  ] [  ] [  ]\r\n[  ] [  ] [  ] [  ]";
             Assert.AreEqual(expectCheckerboard, checkerboard.Print());
         } 
-               
+        
         [Test]
         [Category("CombineTest")]
         public void CornerCombineTest_Should_BeCombinedTo2_When_Place1()
@@ -94,6 +94,24 @@ namespace Service.Test
             game.Combine(checkerboard, 0, 0);
             
             const string expectCheckerboard = "[ 2] [  ] [  ] [  ]\r\n[  ] [  ] [  ] [  ]\r\n[  ] [  ] [  ] [  ]\r\n[  ] [  ] [  ] [  ]";
+            Assert.AreEqual(expectCheckerboard, checkerboard.Print());
+        }
+        
+        [Test]
+        [Category("CombineTest")]
+        public void MultiCombineTest_Should_BeCombinedTo2_When_Place1()
+        {
+            var checkerboard = new[,]
+            {
+                {new Piece(1), new Piece(2), new Piece(3), new Piece()},
+                {new Piece(), new Piece(1), new Piece(), new Piece()},
+                {new Piece(1), new Piece(), new Piece(5), new Piece()},
+                {new Piece(), new Piece(), new Piece(), new Piece()}
+            };
+            var game = new Game();
+            game.Combine(checkerboard, 1, 1);
+            
+            const string expectCheckerboard = "[  ] [  ] [  ] [  ]\r\n[  ] [ 4] [  ] [  ]\r\n[  ] [  ] [ 5] [  ]\r\n[  ] [  ] [  ] [  ]";
             Assert.AreEqual(expectCheckerboard, checkerboard.Print());
         }
     }
